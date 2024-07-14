20535db1bac2489446c698dfc93dd4c3c78cd0f5
Chandan-V-P <ichandanvp@gmail.com>
2024-07-14 20:36:42 +0530

    commit

--- control_flows/for_loop.py
+++ control_flows/for_loop.py
@@ -1,87 +1,22 @@
-#
-# ls = [1,2,3,4,5]
-# ls_squares = []
-#
-# for i in ls:
-#     print("i value is:", i)
-#     ls_squares.append(i*i)
-#
-#
-# print(ls_squares)
-#
-# t = (1,2,3,4,5)
-# t_squares=[]
-#
-# for i in t:
-#     print("i value is:", i)
-#     t_squares.append(i*i)
-#
-# print(t_squares)
+"""This module is to practice for loop in python
+created by Chandan on 14-07-2024"""
 
-# mul=1
-# t = [1,2,3,4,5]
-# for i in t:
-#     mul = mul*i
-#
-# print("mul ", mul)
-#
-# sum =0
-# for i in t:
-#     sum = sum+i
-# print("sum", sum)
-#
-# ls = [1,2,3,4,5,6,7,8,9,10,11,12,14,15,19,100]
-#
-# even_sum = 0
-# odd_sum = 0
-#
-# for i in ls:
-#     if i % 2 == 0:
-#         even_sum = even_sum + i
-#     else:
-#         odd_sum = odd_sum + i
-#
-# print("total sum", even_sum+odd_sum)
-# print("even sum", even_sum)
-# print("odd sum", odd_sum)
-#vowels a,e,i,o,u
-# str1 = 'sky'
-# flag = 0
-# for i in str1.lower():
-#     if i in ['a','e','i','o','u']:
-#         print("vowel present")
-#         break
-#     else:
-#         print("vowel not present")
-#         break
+'''Question 06:
+A student will not be allowed to sit in exam if his/her attendence is less than 75%.
+Take following input from user
+Number of classes held
+Number of classes attended.
+And print
+percentage of class attended
+Is student is allowed to sit in exam or not.
+'''
 
-str1 = 'ETL Automation'  #sKY
-str2=''
-for i in str1:
-    if i.islower():
-        str2 = str2+i.upper()
-    elif i.isupper():
-        str2 = str2+i.lower()
+class_held = int(input("Number of class held: "))
+class_attended = int(input("Number of class attended: "))
 
-print("Str2", str2)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+if (class_attended/class_held)*100 >= 75.0:
+    print("Percentage class attended is:",(class_attended/class_held)*100)
+    print("Congrats, Allowed to sit exam")
+else:
+    print("Percentage class attended is:", (class_attended/class_held)*100)
+    print("Not Allowed to sit exam")
