5a70f619705742255660110b2796d80f8831572e
Chandan-V-P <ichandanvp@gmail.com>
2024-07-16 19:02:45 +0530

    commit

--- control_flows/for_loop.py
+++ control_flows/for_loop.py
@@ -1,22 +1,9 @@
 """This module is to practice for loop in python
 created by Chandan on 14-07-2024"""
 
-'''Question 06:
-A student will not be allowed to sit in exam if his/her attendence is less than 75%.
-Take following input from user
-Number of classes held
-Number of classes attended.
-And print
-percentage of class attended
-Is student is allowed to sit in exam or not.
-'''
+#nested for loop
 
-class_held = int(input("Number of class held: "))
-class_attended = int(input("Number of class attended: "))
-
-if (class_attended/class_held)*100 >= 75.0:
-    print("Percentage class attended is:",(class_attended/class_held)*100)
-    print("Congrats, Allowed to sit exam")
-else:
-    print("Percentage class attended is:", (class_attended/class_held)*100)
-    print("Not Allowed to sit exam")
+for i in range(1,3):
+    for j in range(4,6):
+        for k in range(7,9):
+            print(i,j,k)
---
+++ control_flows/while_loop.py
@@ -0,0 +1,3 @@
+"""This module is created to practice while loop in python
+created by Chandan on 16-07-2024"""
+
