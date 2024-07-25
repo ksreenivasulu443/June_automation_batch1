3faeb00e85def7f9569d7aaa289ae0b882f492a7
Sreeni <kattubadi426@gmail.com>
2024-07-25 17:05:36 +0530

    oops

--- oops_concepts/oops_methods.py
+++ oops_concepts/oops_methods.py
@@ -6,9 +6,8 @@ class Calc:
         self.a = a  # instance variable
         self.b = b  # instance variable
         print("id of self", id(self))
-
-
     def add(self): # instance method
+        self.c=360
         return self.a + self.b
 
     def sub(self): # instance method
@@ -28,27 +27,39 @@ class Calc:
 obj = Calc(4, 5)
 print("id of obj", id(obj))
 
-print(obj.aread_of_circle(3))
+print(obj.__dict__)
 
-print(obj.area_square(4,5))
+print(obj.add())
+
+print(obj.__dict__)
+
+obj.d=560
+
+print(obj.__dict__)
+
+print(obj.a)
+
+# print(obj.aread_of_circle(3))
+#
+# print(obj.area_square(4,5))
 
 
 
 
-class read_data:
-    read_nosqldb = 'nosql db config' - 1kb
-
-    def __init__(self, type_of_file, type_of_db):
-        self.type_of_file = type_of_file  # instance variable 1gb
-        self.type_of_db = type_of_db  # instance variable 1gb
-
-    def read_file(self):
-       return 'file_data'
-
-    def read_db(self):
-       return 'db_data'
-
-    @classmethod
-    def read_nosqldb(cls):
-        return 'nosqldb data'
-
+# class read_data:
+#     read_nosqldb = 'nosql db config' - 1kb
+#
+#     def __init__(self, type_of_file, type_of_db):
+#         self.type_of_file = type_of_file  # instance variable 1gb
+#         self.type_of_db = type_of_db  # instance variable 1gb
+#
+#     def read_file(self):
+#        return 'file_data'
+#
+#     def read_db(self):
+#        return 'db_data'
+#
+#     @classmethod
+#     def read_nosqldb(cls):
+#         return 'nosqldb data'
+#
